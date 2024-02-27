9a000273ab07b262193cb2dd4574263af2c3bb51
db-Reboot <db.Reboot@protonmail.com>
2024-02-27 12:09:50 +0000

    Added controllers and routers for training & routine

--- router/router.index.js
+++ router/router.index.js
@@ -1,8 +1,12 @@
 const router = require('express').Router()
-const userRouter = require('./user.router')
 const exerciseRouter = require('./exercise.router')
+const routineRouter = require('./routine.router')
+const trainingRouter = require('./training.router')
+const userRouter = require('./user.router')
 
-router.use('/user', userRouter )
 router.use('/exercise', exerciseRouter )
+router.use('/routine', routineRouter )
+router.use('/training', trainingRouter )
+router.use('/user', userRouter )
 
 module.exports = router
---
+++ router/routine.router.js
@@ -0,0 +1,10 @@
+const routineRouter = require('express').Router()
+
+const {
+    createRoutine
+}
+    = require('.././api/controllers/routine.controller')
+
+routineRouter.post('/', createRoutine)
+
+module.exports = routineRouter;
